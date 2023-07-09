f58e4d0b2869ac01adfdb4cd4707326d953dc674
BelousovaMN <mariabelousovacoop@mail.ru>
2023-07-09 15:05:50 +0300

    часть 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 const size_t SCREEN_WIDTH = 80;
@@ -68,6 +69,49 @@ void show_histogram_text(const vector<size_t>& bins) {
     }
 }
 
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "balck") {
+    cout << "<rect x = '" << x << "' y = '" << y << "' width = '" << width << "' height = '" << height 
+         << "' stroke = '" << stroke << "' fill = '" << fill << "'/>";
+};
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT, "#FFFAF0", "#FFD700");
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "#FFFAF0", "#FFD700");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
 int main()
 {
     size_t number_count;
@@ -82,5 +126,5 @@ int main()
     cin >> bin_count;
     const auto bins = make_histogram(numbers, bin_count);
 
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 }
