7a645468273e35cc44a0177e91c4edd7a7ca593d
BelousovaMN <mariabelousovacoop@mail.ru>
2023-07-09 16:57:19 +0300

    часть 3

---
+++ histogram.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max);
---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include "histogram.h"
+#include <cassert>
+
+void test_positive() {
+	double min = 0;
+	double max = 0;
+	find_minmax({ 1, 2, 3 }, min, max);
+	assert(min == 1);
+	assert(max == 3);
+}
+
+void test_negative() {
+	double min = 0;
+	double max = 0;
+	find_minmax({ -1, -2, -3 }, min, max);
+	assert(min == -3);
+	assert(max == -1);
+}
+
+void test_different() {
+	double min = 0;
+	double max = 0;
+	find_minmax({ 1, 1, 1 }, min, max);
+	assert(min == 1);
+	assert(max == 1);
+}
+
+void test_one() {
+	double min = 0;
+	double max = 0;
+	find_minmax({ 1 }, min, max);
+	assert(min == 1);
+	assert(max == 1);
+}
+
+void test_empty() {
+	double min = 0;
+	double max = 0;
+	find_minmax({  }, min, max);
+	assert(min == 0);
+	assert(max == 0);
+}
+
+int main() {
+	test_positive();
+	test_negative();
+	test_different();
+	test_one();
+	test_empty();
+}
